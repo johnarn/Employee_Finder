b5eeffc2527c2af7cdd5f3b089d2e7dea4459b10
John Arnokouros <johnarn@windowslive.com>
2020-08-08 17:08:51 +0300

    Connect the attribute views with the business logic

--- app/src/main/java/com/example/employeefinder/Attributes.java
+++ app/src/main/java/com/example/employeefinder/Attributes.java
@@ -1,14 +1,60 @@
 package com.example.employeefinder;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class Attributes extends AppCompatActivity {
+
+    private Context context = this;
+    private EditText attributeName;
+    private Dialog dialog;
+    private Button btnOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attributes);
+
+        FloatingActionButton btnAdd = findViewById(R.id.btnAdd);
+        dialog = new Dialog(context);
+        dialog.setContentView(R.layout.dialog_create_attribute);
+        dialog.setCancelable(false);
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+
+                if (dialog.isShowing()) {
+                    btnOk = dialog.findViewById(R.id.btnCreateOk);
+                    System.out.println(btnOk != null);
+                    attributeName = dialog.findViewById(R.id.editTextAttributeName);
+
+                    btnOk.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            /*
+                            TODO
+                                -Set new attribute to database
+                                -Check if the attribute exists
+                                -Show the new attribute to ListView
+                             */
+                            System.out.println(attributeName.getText());
+                            dialog.dismiss();
+                        }
+                    });
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/employeefinder/MainActivity.java
+++ app/src/main/java/com/example/employeefinder/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.employeefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAttributes = findViewById(R.id.btnAttributes);
+        Button btnEmployees = findViewById(R.id.btnEmployees);
+        Button btnMap = findViewById(R.id.btnMap);
+
+
+
+        btnAttributes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Attributes.class);
+                startActivity(intent);
+            }
+        });
     }
 }
