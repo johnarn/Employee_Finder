71b1527a90184fa848e75860a9553a8f04105305
John Arnokouros <johnarn@windowslive.com>
2020-08-12 13:09:51 +0300

    Added comments and reformatted the code of MapsActivity

--- app/src/main/java/com/example/employeefinder/MapsActivity.java
+++ app/src/main/java/com/example/employeefinder/MapsActivity.java
@@ -23,6 +23,9 @@ import java.util.Map;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    /**
+     * Initialize variables
+     */
     private GoogleMap mMap;
     private DbController dbController;
     private HashMap<String, Boolean> employees_names;
@@ -32,8 +35,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Initialize the controller of the database
         dbController = new DbController(this);
 
+        // Get from SearchResultActivity the names of the employees to show to the map
         employees_names = (HashMap<String, Boolean>) getIntent().getSerializableExtra("HashMapOfEmployeesNames");
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -50,13 +56,26 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
         Geocoder gc = new Geocoder(this);
         try {
+
+            // Initialize the addresses of the employees
             HashMap<LatLng, Boolean> addresses = new HashMap<>();
+
+            // For every employee
             for (Map.Entry<String, Boolean> employee_name : employees_names.entrySet()) {
-                Map.Entry pair = (Map.Entry) employee_name;
+
+                // Get the address of the employee from database
                 String home_address = dbController.getHomeAddress(employee_name.getKey());
+
+                // Find the 5 most suitable addresses that google returns
                 List<Address> home_addresses = gc.getFromLocationName(home_address, 5);
+
+                // Take the first most suitable address
                 LatLng address = new LatLng(home_addresses.get(0).getLatitude(), home_addresses.get(0).getLongitude());
+
+                // Initialize marker
                 MarkerOptions marker = new MarkerOptions().position(address).title(employee_name.getKey());
+
+                // If the employee is the selected change the color of marker to orange and zoom
                 if (employee_name.getValue()) {
                     marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
                     mMap.addMarker(marker);
@@ -68,24 +87,35 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     addresses.put(address, false);
                 }
 
+                // Make available to user to zoom in and out at the map
                 mMap.getUiSettings().setZoomControlsEnabled(true);
             }
+
+            // Draw lines that connect the selected employee with all the others employees who have the same attribute
             drawPrimaryLinePath(addresses);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
+    /**
+     * Draw lines that connect the selected employee with all the others employees who have the same attribute
+     */
     private void drawPrimaryLinePath(HashMap<LatLng, Boolean> addressHashMap) {
+
+        //Security checks
         if (mMap == null) {
             return;
         }
         if (addressHashMap.size() < 2) {
             return;
         }
+
+        // Initialize variables
         PolylineOptions options = new PolylineOptions();
         LatLng main_address = null;
+
+        // Find the selected employee
         for (Map.Entry<LatLng, Boolean> address : addressHashMap.entrySet()) {
             if (address.getValue()) {
                 main_address = address.getKey();
@@ -93,6 +123,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         }
 
+        // Create and show the lines that connect the selected employee with all the other
         for (Map.Entry<LatLng, Boolean> address : addressHashMap.entrySet()) {
             options.add(main_address);
             options.color(Color.parseColor("#CC0000FF"));
