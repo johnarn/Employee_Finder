2b9df89fcabba5d0019e18ce2abdf7440e1bf4fa
John Arnokouros <johnarn@windowslive.com>
2020-08-12 11:51:58 +0300

    Added comments and reformatted the code of CreateEmployee

--- app/src/main/java/com/example/employeefinder/CreateEmployee.java
+++ app/src/main/java/com/example/employeefinder/CreateEmployee.java
@@ -19,8 +19,10 @@ import java.util.ArrayList;
 
 public class CreateEmployee extends Activity {
 
-    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
-    ArrayAdapter<String> adapterAttributesOfEmployee, adapterAvailableAttributesOfEmployee;
+    /**
+     * Initialize variables
+     */
+    private ArrayAdapter<String> adapterAttributesOfEmployee, adapterAvailableAttributesOfEmployee;
     private Button btnCreateOk;
     private EditText editTextEmployeeName, editTextEmployeeDayOfBirth, editTextEmployeeHomeAddress;
     private RadioGroup radioGroupDriversLicense;
@@ -33,12 +35,17 @@ public class CreateEmployee extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_create_employee);
+
+        // Initialize the database controller
         dbController = new DbController(this);
 
+        // Contains all the available attributes for an employee
         listAvailableAttributes = new ArrayList<>();
+
+        // Contains all the attributes of a certain employee
         listAttributeOfEmployee = new ArrayList<>();
 
-
+        // Initialize all the views from the dialog_create_employee
         editTextEmployeeName = findViewById(R.id.editTextEmployeeName);
         editTextEmployeeDayOfBirth = findViewById(R.id.editTextEmployeeDayOfBirth);
         editTextEmployeeHomeAddress = findViewById(R.id.editTextEmployeeHomeAddress);
@@ -47,10 +54,42 @@ public class CreateEmployee extends Activity {
         listViewAvailableAttributesOfEmployee = findViewById(R.id.listViewAvailableAttributesOfEmployee);
         listViewAttributeOfEmployee = findViewById(R.id.listViewAttributeOfEmployee);
 
+        // Populate the list with all the available attributes from database
+        listAvailableAttributes = dbController.getAttributes_names();
+
+        // Show all the attributes that a certain employee has
+        adapterAttributesOfEmployee = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listAttributeOfEmployee);
+        listViewAttributeOfEmployee.setAdapter(adapterAttributesOfEmployee);
+        listViewAttributeOfEmployee.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                adapterAvailableAttributesOfEmployee.add(adapterAttributesOfEmployee.getItem(position));
+                adapterAttributesOfEmployee.remove(adapterAttributesOfEmployee.getItem(position));
+            }
+        });
+
+        // Show all the available attributes that a certain employee can have
+        adapterAvailableAttributesOfEmployee = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listAvailableAttributes);
+        listViewAvailableAttributesOfEmployee.setAdapter(adapterAvailableAttributesOfEmployee);
+        listViewAvailableAttributesOfEmployee.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                adapterAttributesOfEmployee.add(adapterAvailableAttributesOfEmployee.getItem(position));
+                adapterAvailableAttributesOfEmployee.remove(adapterAvailableAttributesOfEmployee.getItem(position));
+            }
+        });
+        adapterAttributesOfEmployee.notifyDataSetChanged();
+        adapterAvailableAttributesOfEmployee.notifyDataSetChanged();
 
         btnCreateOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Security chekcs
                 if (editTextEmployeeName.getText().toString().matches("")) {
                     Toast.makeText(CreateEmployee.this, "You did not enter a name", Toast.LENGTH_SHORT).show();
                     return;
@@ -68,61 +107,28 @@ public class CreateEmployee extends Activity {
                     return;
                 }
 
-
+                // Find which radio button is checked
                 int radioButtonID = radioGroupDriversLicense.getCheckedRadioButtonId();
                 final RadioButton radioButton = radioGroupDriversLicense.findViewById(radioButtonID);
+
+                // Create a new entry in the Employees table
                 dbController.insertToEmployeesTable(editTextEmployeeName.getText().toString(),
                         radioButton.getText().toString(), editTextEmployeeDayOfBirth.getText().toString(),
                         editTextEmployeeHomeAddress.getText().toString());
 
+                // Connect the employee with all of his attributes
                 for (int i = 0; i < adapterAttributesOfEmployee.getCount(); i++) {
                     dbController.insertToEmployeeAttributeTable(adapterAttributesOfEmployee.getItem(i), editTextEmployeeName.getText().toString());
                 }
 
+                // Sent back to EmployeesActivity the name of the new employee
                 Intent intent = new Intent();
                 intent.putExtra("NAME", editTextEmployeeName.getText().toString());
                 setResult(2, intent);
-                finish();//finishing activity
-            }
 
-        });
-
-        listAvailableAttributes = dbController.getAttributes_names();
-
-        adapterAttributesOfEmployee = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                listAttributeOfEmployee);
-
-        adapterAvailableAttributesOfEmployee = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                listAvailableAttributes);
-
-
-        listViewAttributeOfEmployee = findViewById(R.id.listViewAttributeOfEmployee);
-        listViewAttributeOfEmployee.setAdapter(adapterAttributesOfEmployee);
-        listViewAttributeOfEmployee.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                adapterAvailableAttributesOfEmployee.add(adapterAttributesOfEmployee.getItem(position));
-                adapterAttributesOfEmployee.remove(adapterAttributesOfEmployee.getItem(position));
-
+                // finish the activity
+                finish();
             }
         });
-
-        listViewAvailableAttributesOfEmployee = findViewById(R.id.listViewAvailableAttributesOfEmployee);
-        listViewAvailableAttributesOfEmployee.setAdapter(adapterAvailableAttributesOfEmployee);
-        listViewAvailableAttributesOfEmployee.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                adapterAttributesOfEmployee.add(adapterAvailableAttributesOfEmployee.getItem(position));
-                adapterAvailableAttributesOfEmployee.remove(adapterAvailableAttributesOfEmployee.getItem(position));
-
-
-            }
-        });
-        adapterAttributesOfEmployee.notifyDataSetChanged();
-        adapterAvailableAttributesOfEmployee.notifyDataSetChanged();
-
-
     }
 }
