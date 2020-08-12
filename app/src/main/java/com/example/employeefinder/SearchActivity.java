fa09f75956a726262bb9329322752945aba6ad9e
John Arnokouros <johnarn@windowslive.com>
2020-08-12 12:29:47 +0300

    Added comments and reformatted the code of SearchActivity

--- app/src/main/java/com/example/employeefinder/SearchActivity.java
+++ app/src/main/java/com/example/employeefinder/SearchActivity.java
@@ -2,8 +2,6 @@ package com.example.employeefinder;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -17,31 +15,36 @@ import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
-    SearchView searchView;
-    ListView listView;
-    ArrayList<String> list;
-    ArrayAdapter<String> adapter;
-    private DbController dbController;
+    /**
+     * Initialize variables
+     */
+    private ArrayList<String> list;
+    private ArrayAdapter<String> adapter;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        dbController = new DbController(this);
+
+        // Initialize the controller of the database
+        DbController dbController = new DbController(this);
 
 
-        searchView = findViewById(R.id.searchView);
-        listView = findViewById(R.id.listView);
-        list = new ArrayList<String>();
+        // Initialize the views of the SearchActivity
+        SearchView searchView = findViewById(R.id.searchView);
+        ListView listView = findViewById(R.id.listView);
+
+        // Populate the listView with the attributes names
         list = dbController.getAttributes_names();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
-
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
+
+                // Filter the search query
                 if (list.contains(query)) {
                     adapter.getFilter().filter(query);
                 } else {
@@ -57,13 +60,12 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(SearchActivity.this, SearchResultActivity.class);
-                intent.putExtra("attribute_name", list.get(position).toString());
-                startActivity(intent);
-            }
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+
+            //Go to SearchResultActivity and send the attribute name that has been selected
+            Intent intent = new Intent(SearchActivity.this, SearchResultActivity.class);
+            intent.putExtra("attribute_name", list.get(position));
+            startActivity(intent);
         });
 
 
